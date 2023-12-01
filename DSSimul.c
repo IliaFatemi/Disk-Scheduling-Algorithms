191db582af5a8dc593d7fc700c85b519a69ce3f8
IliaFatemi <fatemiilia4@gmail.com>
2023-12-01 14:07:55 -0800

    Extracted user input into a list

--- DSSimul.c
+++ DSSimul.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 #include "dsa.h"
 
-int main(){
-    
+int main(int argc, char *argv[]){
+    printf("%s\n", argv[1]);
+    int request_list[MIN_REQUESTS];
+    int requested_list_size;
+    char *arg = argv[1];
+    char *ptr = arg;
+        while (*ptr != '\0' && requested_list_size < MIN_REQUESTS) {
+            // Extract integers separated by commas
+            int track = strtol(ptr, &ptr, 10);
+            if (ptr == arg || *ptr != ',' && *ptr != '\0') {
+                printf("Invalid input format.\n");
+                return 1;
+            }
+            request_list[requested_list_size++] = track;
+            if (*ptr == ',') ptr++; // Move past the comma
+        }
+
+
     return 0;
 }
